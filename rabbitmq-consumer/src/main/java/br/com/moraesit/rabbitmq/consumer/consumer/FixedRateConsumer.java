891864affef53523e829b34d3ee024eab096ea0e
jpmoraess <moraess.it@gmail.com>
2022-07-13 21:18:17 -0300

    multiple consumers for each Queue

--- rabbitmq-consumer/src/main/java/br/com/moraesit/rabbitmq/consumer/consumer/FixedRateConsumer.java
+++ rabbitmq-consumer/src/main/java/br/com/moraesit/rabbitmq/consumer/consumer/FixedRateConsumer.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 //@Service
 public class FixedRateConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(FixedRateConsumer.class);
 
-    @RabbitListener(queues = "fixedrate")
-    public void listen(String message) {
-        log.info("consuming {}", message);
+    @RabbitListener(queues = "fixedrate", concurrency = "3-6")
+    public void listen(String message) throws InterruptedException {
+        TimeUnit.MICROSECONDS.sleep(ThreadLocalRandom.current().nextLong(1000, 2000));
+        log.info("{} :  consuming {}", Thread.currentThread().getName(), message);
     }
 }
